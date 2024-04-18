d517c11b1724eb2d5c63b16a13040fdf098d884a
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-04-18 01:13:57 +0100

    bowler animation transitions

---
+++ HypercasualGames/Assets/Game1_Cricket/Scripts/PlayerBowler.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBowler : MonoBehaviour
+{
+    public enum State { Idle, Aiming, Running, Bowling}
+
+    [Header("Elements")]
+    [SerializeField] private Animator bowlerAnimator;
+
+    [Header("Settings")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float runDuration;
+    private float runTimer;
+
+    private State state;
+
+    private void Start()
+    {
+        StartRunning();
+    }
+
+    private void Update()
+    {
+        ManageState();
+    }
+
+    void ManageState()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                break;
+            case State.Running:
+                Run();
+                break;
+            case State.Bowling:
+                StartBowling();
+                break;
+            case State.Aiming:
+                break;
+        }
+    }
+
+    
+
+    private void Run()
+    {
+        transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+
+        runTimer += Time.deltaTime;
+
+        if(runTimer >= runDuration)
+        {
+            state = State.Bowling;
+        }
+    }
+
+    void StartRunning()
+    {
+        state = State.Running;
+        bowlerAnimator.SetInteger("State", 1);
+    }
+
+    void StartBowling()
+    {
+        state = State.Bowling;
+        bowlerAnimator.SetInteger("State", 2);
+    }
+}
